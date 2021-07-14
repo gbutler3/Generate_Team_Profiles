7fea06c2023e0939dc71f34272bb9d4320aae83d
grace butler <gmx.butler@gmail.com>
2021-07-13 22:42:19 -0500

    Pass Intern Tests

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,11 +1,22 @@
 // The other three classes will extend `Employee`.
-const Employee = require("../lib/Employee")
+const Intern = require("../lib/Intern")
 
+describe("Intern class", () => {
+    describe("Intern insantiation", () =>{
+        it("Should create new Intern class", () => {
+            const obj = new Intern("Grace", 0, "example@example.com", "TWU");
+            expect(obj instanceof Intern).toEqual(true);
+        });
 
-// In addition to `Employee`'s properties and methods, `Intern` will also have the following:
+        it("Should return TWU as school ", () => {
+            const obj = new Intern("Grace", 0, "example@example.com", "TWU");
+            expect(obj.getSchool()).toEqual("TWU");
+        });
 
-// * `school`
-// * `getSchool()`
-// * `getRole()`&mdash;overridden to return `'Intern'`
+        it("Should return Intern as Role ", () => {
+            const obj = new Intern("Grace", 0, "example@example.com", "TWU");
+            expect(obj.getRole()).toEqual("Intern");
+        });
 
-
+    });
+});
