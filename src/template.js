3efe5520b3af59f0313e1faeb9615b67f1d92591
grace butler <gmx.butler@gmail.com>
2021-07-14 19:49:44 -0500

    src for js rendering the cards

--- src/template.js
+++ src/template.js
@@ -1,3 +1,45 @@
 function createHTML(allEmployees){
+    let allCards = '';
+    allEmployees.forEach(employee =>{
+        allCards += renderCard(employee);
+    });
 
+    return `<html>
+        <body>
+            ${allCards}
+        </body>
+    </html>`;
 }
+
+function renderCard(employee){
+    switch(employee.gitRole()) {
+        case "Engineer":
+            return `<div>
+            <h2> ${employee.getName()}</h2>
+            <h2> ${employee.gitRole()}</h2>
+            <h4> ID: ${employee.getID()}</h4>
+            <h4> Email: ${employee.getEmail()}</h4>
+            </div>`;
+            break; 
+        
+        case "Intern":
+            return `<div>
+            <h2> ${employee.getName()}</h2>
+            <h2> ${employee.gitRole()}</h2>
+            <h4> ID: ${employee.getID()}</h4>
+            <h4> Email: ${employee.getEmail()}</h4>
+            <h4> Email: ${employee.getSchool()}</h4>
+            </div>`;
+            break; 
+
+        case "Manager":
+            return `<div>
+            <h2> ${employee.getName()}</h2>
+            <h2> ${employee.gitRole()}</h2>
+            <h4> ID: ${employee.getID()}</h4>
+            <h4> Email: ${employee.getEmail()}</h4>
+            <h4> Office Number: ${employee.getOfficeNumber()}</h4>
+            </div>`;
+            break;
+    }
+};
