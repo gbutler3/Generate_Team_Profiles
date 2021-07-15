41715f6bdc61b78b5f1ef49e3c5f5a57bbef16f6
grace butler <gmx.butler@gmail.com>
2021-07-14 22:35:24 -0500

    prompts for all the positions work in the terminal

--- index.js
+++ index.js
@@ -21,55 +21,112 @@ function addTeamMember(){
             message: "What team member would you like to add next?",
             choices: ["Manager", "Engineer", "Intern", "No more team members"]
         },
+    ]).then(choices => {
+        switch (choices.teamRole){
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            case "No more team members":
+                createHTML(); //took this from the templatejs. not sure where to put this yet 
+                break;
+        }
+    })
+};
+
+function addManager (){
+    inquirer.prompt([
         {
             type: "input",
-            name: "teamName",
+            name: "mName",
             message:"What is the employee's name?",
-            validate: answer => {
-                if (answer === " "){
-                    console.log("please enter the employee's name!")
-                    return false; 
-                } else {
-                    return true;
-                }
-            }
         },
         {
             type: "number",
-            name: "teamId",
+            name: "mID",
             message: "what is the employee's ID?",
-            validate: answer => {
-                if (answer === " "){
-                    console.log("please enter the employee's ID number!")
-                    return false; 
-                } else {
-                    return true;
-                }
-            }
         },
         {
             type: "input",
-            name: "teamEmail",
+            name: "mEmail",
             message: "What is the employee's email?",
-            validate: answer => {
-                if (answer === " "){
-                    console.log("please enter the employee's email!")
-                    return false; 
-                } else {
-                    return true;
-                }
-            }
         },
-
-
-
-        
-    ])
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the employee's office number?",
+        },
+    ]).then(answers => {
+        const m= new Manager(answers.mName, answers.mID, answers.mEmail, answers.officeNumber);
+        teamMembers.push(m);
+        console.log("Member added to the team!")
+        addTeamMember();
+    })
 };
 
-function addEngineer(){};
+function addEngineer (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "eName",
+            message:"What is the employee's name?",
+        },
+        {
+            type: "number",
+            name: "eId",
+            message: "what is the employee's ID?",
+        },
+        {
+            type: "input",
+            name: "eEmail",
+            message: "What is the employee's email?",
+        },
+        {
+            type: "input",
+            name: "Github",
+            message: "What is the employee's Github username?",
+        }
+    ]).then(answers => {
+        const e= new Engineer(answers.eName, answers.eId, answers.eEmail, answers.Github);
+        teamMembers.push(e);
+        console.log("Member added to the team!")
+        addTeamMember();
+    })
+};
 
-function addIntern(){};
-
+function addIntern (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "iName",
+            message:"What is the employee's name?",
+        },
+        {
+            type: "number",
+            name: "iId",
+            message: "what is the employee's ID?",
+        },
+        {
+            type: "input",
+            name: "iEmail",
+            message: "What is the employee's email?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the employee's school?",
+        }
+    ]).then(answers => {
+        const i= new Intern(answers.iName, answers.iId, answers.iEmail, answers.school);
+        teamMembers.push(i);
+        console.log("Member added to the team!")
+        addTeamMember();
+    })
+};
 
 init();
